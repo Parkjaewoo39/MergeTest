8244ac33ce1814d0c45ee54bce3bcb07560c5ccc
DESKTOP-ILDHSHN\KGA_11 <wodn625@naver.com>
2023-03-08 12:12:19 +0900

    [Gamma] Add SetInch() funtion

--- Merge/Program.cs
+++ Merge/Program.cs
@@ -6,12 +6,34 @@ namespace Merge
     {
         static void Main(string[] args)
         {
-            Ruler ruler = new Ruler();
+            Ruler ruler = new Ruler(10);
+
+            //Beta add Run()
+            ruler.Run();
         }
     }
 
-    public class Ruler 
+    //Alpha Ruler add
+    public class Ruler
     {
+        private const float ONE_INCH = 2.54f;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            //{Gamma 2023.03.08 private SetInch 함수를 새로 추가/ Gamma
+            private set { this.SetInch(value); }
+
+        }
+        public Ruler(int Value) { Centimeter = Value; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm는 {this.Inch}inch 입니다.");
+        }
+
+        private void SetInch(float inchValue) { Centimeter = (int)(inchValue / ONE_INCH); }
+        //}Gamma 2023.03.08 private SetInch 함수를 새로 추가/ Gamma
 
     }
 }

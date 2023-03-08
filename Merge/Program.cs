80b84542f12865872c641d1a587cce811ab50433
DESKTOP-ILDHSHN\KGA_11 <wodn625@naver.com>
2023-03-08 11:29:21 +0900

    [Main] Setup

---
+++ Merge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Merge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler();
+        }
+    }
+
+    public class Ruler 
+    {
+
+    }
+}
